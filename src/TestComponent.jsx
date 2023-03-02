1b8838055598f3c28ca2bde34eccb559db79b95f
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-03-02 13:47:43 +0800

    use css modules + regular css file + tailwind css for each component's styling

--- src/TestComponent.jsx
+++ src/TestComponent.jsx
@@ -1,8 +1,15 @@
 import './tailwindStyles.css';
 import './TestComponent.css';
+import styles from './TestComponent.module.css';
+
+console.log(styles);
 
 const TestComponent = () => {
-  return <div className="black-bold-font bg-red-700">Test Component</div>;
+  return (
+    <div className={`${styles['bold-font-color']} border-style bg-red-700`}>
+      Test Component
+    </div>
+  );
 };
 
 export default TestComponent;
