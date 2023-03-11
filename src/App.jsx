f3d78893c9e8dfc9a5131bebda6673a924cecf0e
Jojo <reustaquio@users.noreply.github.com>
2023-03-11 15:43:29 +0800

    add project header (#19)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Shared/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Footer from './components/Shared/Footer';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
             {/* Add Projects, Resume Components*/}
           </Routes>
         </main>
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+const Projects = () => {
+  return (
+    <section>
+      <div className="text-center text-4xl">Projects</div>
+      <span className="text-1xl text-center">
+        <p>Some things I have built</p>
+      </span>
+    </section>
+  );
+};
+
+export default Projects;
